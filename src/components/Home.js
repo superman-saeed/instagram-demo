2eb3f5b1f993c18866a19688afc6c5216df1d9ec
superman-saeed <salamwahab@gmail.com>
2019-03-28 02:03:35 -0400

    added userbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,14 +6,14 @@ class Home extends React.Component {
   render(){
     return (
       <div>
-      <div className="toolbar">
+      <div className="toolbar top">
       <a href="#pic"><Icon name="camera" size="large"  size="large"/></a>
       <h3>Instagram</h3>
       <a href="#send"><Icon name="send" size="large"  size="large"/></a>
       <a href="#tv"><Icon name="tv"  size="large"/></a>
       </div>
       <div className="feed">
-      <h2>Feeds</h2> 
+      <h2>Feeds</h2>
       <p>App is still in progress</p> </div>
       </div>
     );
--- src/components/User.js
+++ src/components/User.js
@@ -1,11 +1,17 @@
 import React from "react";
+import {Icon} from "semantic-ui-react";
+import "../stylesheets/user.css";
 
 
 class User extends React.Component {
   render(){
     return (
       <div className="">
-      <h1> User </h1>
+      <div className="user top">
+        <h3>superman-saeed</h3>
+        <a href="#archive"><Icon name="bars" size="large" /></a>
+        <a href="#menu"><Icon name="history" size="large" /></a>
+      </div>
       <p>App still in progress</p>
       </div>
     );
