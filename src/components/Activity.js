2eef19c752c9420a62d5468d8e5be7ffd63e900a
superman-saeed <salamwahab@gmail.com>
2019-04-07 08:40:10 -0400

    fixed activity tab

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -1,41 +1,66 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import You from "./You";
 import Following from "./Following";
 import "../stylesheets/activity.css";
 
 
+
 class Activity extends React.Component {
+  constructor(props){
+    super(props);
+    this.state ={
+      hash:"following",
+      ftab : "tab",
+      ytab: "",
+    }
+
+  }
+  route=(e)=>{
+    e.preventDefault();
+    this.element = e.currentTarget;
+     switch (e.currentTarget.hash) {
+       case "#you":
+          this.setState({
+            hash:"you",
+            ytab:"tab",
+            ftab:""});
+
+         break;
+         case "#following":
+          this.setState({
+              hash:"following",
+              ytab:"",
+              ftab:"tab"});
+          break;
+       default:
+
+   }
+  }
   render(){
     const {url} = this.props.match;
     return (
-      <Router>
       <div className="activity">
        <div className="activity-nav">
        <ul>
-       <li><Link to={`${url}/following`}>Following</Link></li>
-       <li><Link to={`${url}/you`}>you</Link></li>
+       <li>
+       <a className={this.state.ftab}
+       href="#following" onClick={this.route}>Following</a>
+       </li>
+       <li>
+       <a className={this.state.ytab}
+       href="#you" onClick={this.route}> you</a>
+       </li>
        </ul>
        </div>
+       <div>
+        {(this.state.hash=="following") ? <Following /> : <You />}
       </div>
-      <Route  path={`${url}/:id`} component={SwitchTabs} />
-      <Route  exact path={url} component={Following} />
-     </Router>
+      </div>
     );
   }
+
 }
 
-function SwitchTabs({match}){
-  const route = match.params.id;
- console.log(route);
-  switch(route) {
-    case "you":
-      return <You />;
-    case "following":
-      return <Following />;
-    default:
-      return <Following />;
 
-  }
-}
+
 export default Activity;
