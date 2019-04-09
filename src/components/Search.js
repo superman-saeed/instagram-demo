097be965b7ae437d05d510f86b4fe0c016248081
superman-saeed <salamwahab@gmail.com>
2019-04-09 12:53:56 -0400

    fixed searchgrid

--- src/components/Search.js
+++ src/components/Search.js
@@ -24,43 +24,18 @@ function PhotoGrid(props){
   const {p,num} = props;
   return(
     <div className="photo-grid">
-     <ThreeGrid num={1}/>
-     <LeftGrid />
-     <MiddleGrid />
-     <ThreeGrid num={4}/>
+     <div className="g-photo"><div className="g-one"> 1</div></div>
+     <div className="g-photo"><div className="g-one"> 2</div></div>
+     <div className="g-photo"><div className="g-one"> 3</div></div>
+     <div className="g-photo"><div className="g-one"> 4</div></div>
+     <div className="g-photo db-grid">
+    <div className="db-inner"><div>6</div></div>
+    <div className="db-inner"><div>6</div></div>
+    </div>
+     <div className="g-photo"><div className="g-one"> 7</div></div>
+     <div className="g-photo"><div className="g-one"> 8</div></div>
+     <div className="g-photo"><div className="g-one"> 9</div></div>
     </div>
   )
 
-}
-
-function ThreeGrid(props){
-  return(
-    <div className="small-g">
-      <div className="sg-inner">
-        <div className="fthree-glid"><div>{props.num}</div></div>
-        <div className="fthree-glid"><div>{props.num}</div></div>
-        <div className="fthree-glid"><div>{props.num}</div></div>
-      </div>
-    </div>
-  );
-}
-
-function MiddleGrid(props){
-  return(
-    <div className="big-g">
-    <div className="sg-inner">
-      <div className="fone-grid"><div>3</div></div>
-    </div>
-    </div>
-  );
-}
-function LeftGrid(props){
-  return(
-    <div className="left-g">
-    <div className="inner">
-      <div className="ftwo-glid"><div>2</div></div>
-      <div className="ftwo-glid"><div>2</div></div>
-    </div>
-    </div>
-  );
 }
